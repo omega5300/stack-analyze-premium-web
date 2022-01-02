f8da8aabdd734e5005b71c1ef4e3491ffe7440fb
omega5300 <omegamatrix5351@gmail.com>
2022-01-01 20:38:37 -0500

    tech api

--- pages/api/tech.js
+++ pages/api/tech.js
@@ -1,18 +1,23 @@
+// moduled
 import Wappalyzer from 'wappalyzer'
 
 export default async function handler(req, res) {
+  // start
   const wappalyzer = await new Wappalyzer;
+  
+  const { url } = req.query
   
   try {
     await wappalyzer.init();
     
-    const { technologies } = await wappalyzer.open('https://example.com').analyze();
+    const { technologies } = await wappalyzer.open(url).analyze();
     
     res.status(200).json(technologies)
   } catch(err) {
     res.status(500).json({ msg: err.message })
   }
   
+  // finish
   await wappalyzer.destroy();
 }
 
